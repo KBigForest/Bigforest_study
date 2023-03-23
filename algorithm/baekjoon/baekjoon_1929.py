926343fcfaea079460a670ef66044af77bc50861
KBigForest <gksfka12@naver.com>
2023-03-23 10:05:09 +0900

    소수구하기

--- algorithm/baekjoon/baekjoon_1929.py
+++ algorithm/baekjoon/baekjoon_1929.py
@@ -1,5 +1,15 @@
-import sys
+import math
+M,N = map(int, input().split())
+a = [True]*(N+1)
+a[0] = False
+a[1] = False
+n = int(math.sqrt(N))
 
-M,N = map(int, sys.stdin.readline().split())
-
-a = [print(i) for i in range(M,N+1) if i % j != 0 for j in range(M,N+1)]
+for i in range(2,n+1):
+    if a[i] == True:
+        for j in range(2*i, N+1, i):
+            a[j] = False
+            
+for i in range(M,N+1):
+    if a[i]:
+        print(i)
