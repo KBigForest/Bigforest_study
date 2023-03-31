640b2c69efa48d0e998cf0d58113ddb5b3ecaa52
KBigForest <gksfka12@naver.com>
2023-03-31 09:21:57 +0900

    3월 31일 백준포함

---
+++ algorithm/baekjoon/baekjoon_1026.py
@@ -0,0 +1,15 @@
+from itertools import *
+input_num = int(input())
+A = [int(i) for i in input().split()]
+B = [int(i) for i in input().split()]
+sum_list = []
+sum_x = 0
+
+A.sort(lambda x : )
+B.
+
+for i in range(input_num):
+    x = A[i]*B[i]
+    sum_x += x
+    
+print(sum_x)
--- algorithm/baekjoon/baekjoon_1541.py
+++
@@ -1,9 +0,0 @@
-line1 = input()
-line1
-num = []
-plusminus = []
-for i in line1:
-    if i == '+' or i == '-' :
-        plusminus.append(i)
-    else:
-        num.append(i)
