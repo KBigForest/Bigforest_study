699e9f23a10d3c6e755218748da951219a3904ee
KBigForest <gksfka12@naver.com>
2023-03-31 09:44:31 +0900

    백준 1026

--- algorithm/baekjoon/baekjoon_1026.py
+++ algorithm/baekjoon/baekjoon_1026.py
@@ -1,15 +1,18 @@
-from itertools import *
 input_num = int(input())
 A = [int(i) for i in input().split()]
 B = [int(i) for i in input().split()]
 sum_list = []
-sum_x = 0
 
-A.sort(lambda x : )
-B.
+A.sort(reverse=True) # reverse
+      
+B.sort()
 
+sum_x =0
+x=0
 for i in range(input_num):
     x = A[i]*B[i]
     sum_x += x
-    
+
+A
+B    
 print(sum_x)
