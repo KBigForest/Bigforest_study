27b6c9ffd4e26538595ccd932d483c4fb0a95103
KBigForest <gksfka12@naver.com>
2023-03-31 18:58:24 +0900

    text 생성기 알고리즘 등등

--- algorithm/baekjoon/baekjoon_1026.py
+++ algorithm/baekjoon/baekjoon_1026.py
@@ -3,8 +3,7 @@ A = [int(i) for i in input().split()]
 B = [int(i) for i in input().split()]
 sum_list = []
 
-A.sort(reverse=True) # reverse
-      
+A.sort(reverse=True) # reverse 
 B.sort()
 
 sum_x =0
@@ -13,6 +12,4 @@ for i in range(input_num):
     x = A[i]*B[i]
     sum_x += x
 
-A
-B    
 print(sum_x)
